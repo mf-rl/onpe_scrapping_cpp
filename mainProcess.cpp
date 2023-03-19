2e6f0143b6b687cd16640e9e6d9598a1a4d726b3
YawarPandar <mf.ramirezl@gmail.com>
2023-03-19 14:46:39 -0500

    Reading config json

--- mainProcess.cpp
+++ mainProcess.cpp
@@ -1,5 +1,19 @@
 #include "mainProcess.h"
+#include <json/json.h>
+#include <fstream>
 
 void MainProcess::ExecuteProcess(char opt, char tip_pro, char sel, string mesa_sel){
-    
+    _opt = opt;
+    _tip_pro = tip_pro;
+    _sel = sel;
+    _mesa_sel = mesa_sel;
+
+    Json::Value settings;
+    std::ifstream settings_file("appSettings.json", std::ifstream::binary);
+    settings_file >> settings;
+
+    Json::Value _endPointSet = _opt == ProcesarPrimeraV ? settings["Api"]["First"] : settings["Api"]["Second"];
+
+    cout << _endPointSet["Title"] << endl;
+
 }
--- mainProcess.h
+++ mainProcess.h
@@ -5,6 +5,11 @@
 class MainProcess{
     public:
         void ExecuteProcess(char opt, char tip_pro, char sel, string mesa_sel);
+    private:
+        char _opt{'0'};
+        char _tip_pro{'0'};
+        char _sel{'0'};
+        string _mesa_sel{};
 };
 
 #endif
